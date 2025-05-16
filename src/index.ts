69e8899fa19e5030f82c974ad91560f0398a0993
iam4x <iam4x@me.com>
2025-05-16 23:56:40 +0400

    feat(tickers): batch updates in ~50ms

--- src/index.ts
+++ src/index.ts
@@ -149,6 +149,40 @@ const resetPongTimeout = () => {
   }, 5000);
 };
 
+let clearTickerUpdatesBacklogTimeout: NodeJS.Timeout | undefined;
+
+const clearTickerUpdatesBacklog = () => {
+  const toSend: Record<string, Record<string, any>> = {};
+
+  tickerUpdatesBacklog.forEach((update, idx) => {
+    if (toSend[update.data.symbol]) {
+      toSend[update.data.symbol].cs = update.data.cs;
+      toSend[update.data.symbol].ts = update.data.ts;
+      Object.assign(toSend[update.data.symbol].data, update.data);
+    } else {
+      toSend[update.data.symbol] = update;
+    }
+
+    // if the update is the last one for this symbol, we send it
+    if (tickerUpdatesBacklog.lastIndexOf(update) === idx) {
+      server.publish(update.topic, JSON.stringify(toSend[update.data.symbol]));
+    }
+  });
+
+  logger.debug(
+    `Cleared ${tickerUpdatesBacklog.length} backlog entries into ${Object.keys(toSend).length} updates`,
+  );
+
+  // clear backlog array
+  tickerUpdatesBacklog.length = 0;
+
+  // schedule ticker update in 50ms
+  clearTickerUpdatesBacklogTimeout = setTimeout(
+    () => clearTickerUpdatesBacklog(),
+    50,
+  );
+};
+
 const pingObString = `{"op":"ping"}`;
 const onOpen = () => {
   const topics = Object.keys(BYBIT_SUBSCRIBED_TOPICS);
@@ -163,6 +197,11 @@ const onOpen = () => {
     bybitWs.send(pingObString);
     resetPongTimeout();
   }, 10_000);
+
+  clearTickerUpdatesBacklogTimeout = setTimeout(
+    () => clearTickerUpdatesBacklog(),
+    50,
+  );
 };
 
 bybitWs.addEventListener("open", onOpen);
@@ -189,10 +228,55 @@ const onClose = () => {
     unsubscribeBybitTimeouts.forEach((timeout) => clearTimeout(timeout));
     unsubscribeBybitTimeouts.length = 0;
   }
+
+  if (clearTickerUpdatesBacklogTimeout) {
+    clearTimeout(clearTickerUpdatesBacklogTimeout);
+    clearTickerUpdatesBacklogTimeout = undefined;
+  }
 };
 
 bybitWs.addEventListener("close", onClose);
 bybitWs.addEventListener("down", onClose);
+
+const tickerUpdatesBacklog: Record<string, any>[] = [];
+const handleTickerUpdate = (data: Record<string, any>) => {
+  tickerUpdatesBacklog.push(data);
+
+  if (BYBIT_TOPICS_SNAPSHOTS[data.topic]) {
+    Object.assign(BYBIT_TOPICS_SNAPSHOTS[data.topic], data.data);
+  }
+};
+
+const handleOrderBookUpdate = (data: Record<string, any>) => {
+  const orderBook = data.data as Record<string, string[][]>;
+  const snapshot = BYBIT_TOPICS_SNAPSHOTS[data.topic] as Record<
+    string,
+    string[][]
+  >;
+
+  Object.entries(orderBook).forEach(([side, orders]) => {
+    if (side !== "a" && side !== "b") return;
+
+    orders.forEach((order) => {
+      const index = snapshot[side].findIndex((o) => o[0] === order[0]);
+      const amount = parseFloat(order[1]);
+
+      if (index === -1 && amount > 0) {
+        snapshot[side].push(order);
+        return;
+      }
+
+      if (index !== -1 && amount === 0) {
+        snapshot[side].splice(index, 1);
+        return;
+      }
+
+      if (index !== -1 && amount > 0) {
+        snapshot[side][index][1] = order[1];
+      }
+    });
+  });
+};
 
 bybitWs.addEventListener("message", (event) => {
   if (event.data.includes("pong")) {
@@ -205,49 +289,23 @@ bybitWs.addEventListener("message", (event) => {
     const data = JSON.parse(event.data);
 
     if (typeof data.topic === "string") {
-      server.publish(data.topic, event.data);
-
-      if (data.type === "snapshot") {
-        BYBIT_TOPICS_SNAPSHOTS[data.topic] = data.data;
+      // receiving a tickers update, we will batch those in 50ms updates
+      if (data.topic.includes("tickers.") && data.type === "delta") {
+        handleTickerUpdate(data);
         return;
       }
 
-      if (data.topic.startsWith("tickers.") && data.type === "update") {
-        if (BYBIT_TOPICS_SNAPSHOTS[data.topic]) {
-          Object.assign(BYBIT_TOPICS_SNAPSHOTS[data.topic], data.data);
-        }
+      // receiving a snapshot, we will store it in memory
+      if (data.type === "snapshot") {
+        BYBIT_TOPICS_SNAPSHOTS[data.topic] = data.data;
+      }
+      // receiving a orderbook delta, we will update the orderbook in memory
+      else if (data.topic.includes("orderbook.") && data.type === "delta") {
+        handleOrderBookUpdate(data);
       }
 
-      if (data.topic.startsWith("orderbook.") && data.type === "delta") {
-        const orderBook = data.data as Record<string, string[][]>;
-        const snapshot = BYBIT_TOPICS_SNAPSHOTS[data.topic] as Record<
-          string,
-          string[][]
-        >;
-
-        Object.entries(orderBook).forEach(([side, orders]) => {
-          if (side !== "a" && side !== "b") return;
-
-          orders.forEach((order) => {
-            const index = snapshot[side].findIndex((o) => o[0] === order[0]);
-            const amount = parseFloat(order[1]);
-
-            if (index === -1 && amount > 0) {
-              snapshot[side].push(order);
-              return;
-            }
-
-            if (index !== -1 && amount === 0) {
-              snapshot[side].splice(index, 1);
-              return;
-            }
-
-            if (index !== -1 && amount > 0) {
-              snapshot[side][index][1] = order[1];
-            }
-          });
-        });
-      }
+      // forward all bybit messages that weren't caught before
+      server.publish(data.topic, event.data);
     }
   } catch {
     // do nothing, we didnt receive JSON string
